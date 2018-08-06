feab2373e30c5e601dc526558890e7ba14f93882
Joe Hoffmann <josephjohnhoffmann@gmail.com>
2018-08-06 16:49:15 -0500

    Updated

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,17 +3,17 @@ var friendData = [
   {
     friendName: "Doug",
     friendPhoto: "https://en.wikipedia.org/wiki/Beagle#/media/File:Beagle_600.jpg",
-    friendScore: [1, 2, 1, 1, 3, 4, 1, 5, 1, 2]
+    friendScore: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
     {
     friendName: "Bill",
     friendPhoto: "https://en.wikipedia.org/wiki/Beagle#/media/File:Beagle_600.jpg",
-    friendScore: [4, 3, 5, 2, 3, 4, 5, 1, 2, 4]
+    friendScore: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
     {
     friendName: "Sara",
     friendPhoto: "https://en.wikipedia.org/wiki/Beagle#/media/File:Beagle_600.jpg",
-    friendScore: [2, 3, 2, 1, 2, 3, 3, 2, 1, 2]
+    friendScore: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   }
 ];
 
