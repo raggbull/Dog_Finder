2b967d650beee1a3c6938ea9d3739f7b455ac68e
Joe Hoffmann <josephjohnhoffmann@gmail.com>
2018-08-06 21:23:53 -0500

    Dogs

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,17 +2,17 @@
 var friendData = [
   {
     friendName: "Doug",
-    friendPhoto: "https://en.wikipedia.org/wiki/Beagle#/media/File:Beagle_600.jpg",
+    friendPhoto: "https://images.pexels.com/photos/149428/pexels-photo-149428.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350",
     friendScore: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
     {
     friendName: "Bill",
-    friendPhoto: "https://en.wikipedia.org/wiki/Beagle#/media/File:Beagle_600.jpg",
+    friendPhoto: "https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?auto=compress&cs=tinysrgb&h=350",
     friendScore: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
     {
     friendName: "Sara",
-    friendPhoto: "https://en.wikipedia.org/wiki/Beagle#/media/File:Beagle_600.jpg",
+    friendPhoto:"https://images.pexels.com/photos/733416/pexels-photo-733416.jpeg?auto=compress&cs=tinysrgb&h=350",
     friendScore: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   }
 ];
